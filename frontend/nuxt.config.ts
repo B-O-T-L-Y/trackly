cf1def54691c8222cea8d5c8d42e2d432fe235ea
B-O-T-L-Y <agnoter@gmail.com>
2025-11-27 19:26:00 +0200

    feat: install Tailwind CSS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    runtimeConfig: {
-        apiBaseServer: '',
-        apiTokenPath: '',
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
 
-        public: {
-            apiBaseClient: '',
-            statsPollInterval: '',
-        }
-    },
+  runtimeConfig: {
+      apiBaseServer: '',
+      apiTokenPath: '',
+
+      public: {
+          apiBaseClient: '',
+          statsPollInterval: '',
+      }
+  },
+
+  modules: ['@nuxtjs/tailwindcss'],
 })
